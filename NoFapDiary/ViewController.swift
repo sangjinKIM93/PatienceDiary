4e66464e897434d62c2da8af7afdf5c71098483b
sangjinKIM93 <larger611@gmail.com>
2020-08-30 22:05:09 +0900

    Initial Commit

---
+++ NoFapDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoFapDiary
+//
+//  Created by 김상진 on 2020/08/30.
+//  Copyright © 2020 kipCalm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
